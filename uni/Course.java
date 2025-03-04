575a68a9475de3bb563e842c72d716b6cecf5f7f
goli <shirafkanzeynab63@gmail.com>
2025-03-04 20:09:34 +0330

    add Course and PresentedCourse methods

--- uni/Course.java
+++ uni/Course.java
@@ -7,4 +7,27 @@ public class Course {
     static ArrayList<Course> courseList = null;
     String title;
     int units;
+
+    public Course( String newTitle, int newUnits){
+        title = newTitle;
+        units = newUnits;
+
+        if(courseList == null)
+            courseList = new ArrayList<>();
+
+        courseList.add(this);
+        courseID = courseList.size();
+    }
+
+    public static Course findById(int ID){
+        if(courseList != null){
+            for(Course course : courseList){
+                if(course.courseID == ID){
+                    return course;
+                }
+            }
+        }
+        return null;
+    }
+
 }
--- uni/PresentedCourse.java
+++ uni/PresentedCourse.java
@@ -4,9 +4,35 @@ import java.util.ArrayList;
 
 public class PresentedCourse {
     int peresentedCourseID;
-    static ArrayList<PresentedCourse> presentedCourseList;
+    static ArrayList<PresentedCourse> presentedCourseList = null;
     int courseID;
     int professorID;
     int maxCapacity;
     ArrayList<Integer> studentIDList = null;
+
+        public PresentedCourse(int newCourseID, int newProfessorID, int newMaxCapacity) {\
+            courseID = newCourseID;
+            professorID = newProfessorID;
+            maxCapacity = newMaxCapacity;
+
+            courseID = presentedCourseList.size();
+        }
+
+        public static PresentedCourse findById(int ID){
+            if(presentedCourseList != null){
+                for(PresentedCourse presentedCourse : presentedCourseList){
+                    if(presentedCourse.courseID == ID){
+                        return presentedCourse;
+                    }
+                }
+            }
+            return null;
+    }
+
+    public void addStudent(int newStudentID){
+            if(studentIDList.size() <= maxCapacity)
+                studentIDList.add(newStudentID);
+
+            else System.out.println("Capacity is complete. you cant add anyone else.")
+    }
 }
